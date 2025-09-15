9f28a4ff8b34bfe2c092848add76f1827d7ba1ab
Vatsala <vats15shekhar@gmail.com>
2025-09-15 11:26:59 +0530

    Starting the User Sign In Sign Up Project

---
+++ src/main/java/com/personal/UserSignupSignin/UserSignupSigninApplication.java
@@ -0,0 +1,14 @@
+package com.personal.UserSignupSignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSignupSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSignupSigninApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/personal/UserSignupSignin/UserSignupSigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.UserSignupSignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSignupSigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
